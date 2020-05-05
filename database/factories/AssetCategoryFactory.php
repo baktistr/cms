7446abaf8ccd1434faa901fcc49df3514dc1d1a2
hananloser <hasyrawi@gmail.com>
2020-05-05 19:33:52 +0800

    init migration :tada:

---
+++ database/factories/AssetCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AssetCategory;
+use Faker\Generator as Faker;
+
+$factory->define(AssetCategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
