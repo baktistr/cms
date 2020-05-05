1d6f37fe423c0c20d4f7fe670bfeb2db6a976eb9
hananloser <hasyrawi@gmail.com>
2020-05-06 06:30:15 +0800

    delete palyground on web

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/csv', function () {
-    $path = database_path('seeds\locations\districs.csv');
-        $data = file($path);
-        $newData = array();
-        foreach ($data as $row) {
-            $explode = explode(',', $row);
-            $newData[] = $explode;
-        }
-        collect($newData)
-            ->each(function ($pro) {
-                factory(Regency::class)->create([
-                    'id' => $pro[0],
-                    'province_id' => $pro[1],
-                    'name' => $pro[2]
-                ]);
-            });
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
