a236a4c39c0a05db73af99d7399ec19fdce141c9
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-09 07:57:19 +0800

    Setup laravel sanctum.

--- app/User.php
+++ app/User.php
@@ -8,13 +8,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Nova\Actions\Actionable;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class User extends Authenticatable implements HasMedia
 {
-    use Notifiable, Actionable, SoftDeletes, InteractsWithMedia;
+    use Notifiable, Actionable, SoftDeletes, InteractsWithMedia, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
