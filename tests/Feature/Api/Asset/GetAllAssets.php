db86514b2e494a5e75530695c63ab4c80ef02859
hananloser <hasyrawi@gmail.com>
2020-06-04 02:55:35 +0800

    filter test and search test

--- tests/Feature/Api/Asset/GetAllAssets.php
+++ tests/Feature/Api/Asset/GetAllAssets.php
@@ -5,14 +5,33 @@ namespace Tests\Feature\Api\Asset;
 use App\Asset;
 use Laravel\Sanctum\Sanctum;
 use App\User;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class GetAllAssets extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
     /** @test */
     public function can_get_all_assets()
+    {
+
+        $user = factory(User::class)->create([
+            'name'         => 'Old name',
+            'email'        => 'old_email@example.com',
+            'phone_number' => '+6285110374321',
+        ]);
+
+        $user = User::first();
+        Sanctum::actingAs($user);
+        $response = $this->getJson('api/assets/');
+        $response
+            ->assertJsonFragment(["data" => []])
+            ->assertOk();
+    }
+    /**
+     * @test
+     */
+    public function get_filter_asset()
     {
         $user = factory(User::class)->create([
             'name'         => 'Old name',
@@ -20,11 +39,35 @@ class GetAllAssets extends TestCase
             'phone_number' => '+6285110374321',
         ]);
 
+        $user = User::first();
+
         Sanctum::actingAs($user);
 
-        $response = $this->getJson('/api/assets/');
+        $response = $this->getJson('api/assets/filter?category=tanah');
 
-        $response->assertOk();
+        $response
+            ->assertJsonFragment(["data" => []])
+            ->assertOk();
+    }
+    /**
+     * @test
+     */
+    public function get_search_asset()
+    {
+        $user = factory(User::class)->create([
+            'name'         => 'Old name',
+            'email'        => 'old_email@example.com',
+            'phone_number' => '+6285110374321',
+        ]);
+
+        $user = User::first();
+        Sanctum::actingAs($user);
+
+        $response = $this->getJson('api/assets/search?search=ad');
+        $response
+            ->assertJsonFragment(["data" => []])
+            ->assertOk();
+
     }
 
     /**
@@ -33,6 +76,7 @@ class GetAllAssets extends TestCase
     public function get_asset_without_login()
     {
         $response = $this->getJson('api/assets/');
+
         $response->assertUnauthorized();
     }
 }
