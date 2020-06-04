8d40cf3b7216f3de5f6ac01ab3f5117e905e6440
hananloser <hasyrawi@gmail.com>
2020-06-04 22:03:14 +0800

    change response token

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -21,12 +21,18 @@ class LoginController extends Controller
     {
         $user = User::where('email', $request->email)->first();
 
-        if (! $user || ! Hash::check($request->password, $user->password)) {
+        if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
 
-        return $user->createToken($request->device_name)->plainTextToken;
+        // return $user->createToken($request->device_name)->plainTextToken
+
+        return [
+            'success' => 'true',
+            'message' => "",
+            'token'   => $user->createToken($request->device_name)->plainTextToken
+        ];
     }
 }
