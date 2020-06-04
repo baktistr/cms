937acca4a22318fe6fc702966976fd0e31002131
hananloser <hasyrawi@gmail.com>
2020-06-04 17:58:30 +0800

    remove not used import

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [RegisterController::class, 'register'])->name('register');
-
-
 /**
  * Authentication routes...
  */
@@ -33,7 +30,7 @@ Route::prefix('auth')
         // Auth routes for guest...
         Route::middleware('guest')->group(function () {
             Route::post('login', [LoginController::class, 'login'])->name('login');
-            Route::post('register', [RegisterController::class, 'register'])->name('register');
+            Route::post('register', [RegisterController::class , 'register'])->name('register');
             Route::get('verify', [VerificationController::class, 'verify'])->name('verify');
 
             // Forgot password routes...
