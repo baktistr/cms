87b7fd2997be0bdb6fc3ffec707e10c8a9db565b
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-06-06 18:42:59 +0800

    Implement multi login credential using email or username.

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -5,11 +5,52 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
 use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
+    use AuthenticatesUsers;
+
+    /**
+     * Login username.
+     *
+     * @var [type]
+     */
+    private $username;
+
+    public function __construct()
+    {
+        $this->username = $this->findUsername();
+    }
+
+    /**
+     * Find login credential using username or email.
+     *
+     * @return string
+     */
+    private function findUsername()
+    {
+        $login = request()->input('email');
+
+        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        request()->merge([$fieldType => $login]);
+
+        return $fieldType;
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return $this->username;
+    }
+
     /**
      * Handle an API login request to the application.
      *
@@ -19,7 +60,7 @@ class LoginController extends Controller
      */
     public function login(LoginRequest $request)
     {
-        $user = User::where('email', $request->email)->first();
+        $user = User::where($this->username, $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
