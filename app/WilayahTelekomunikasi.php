634157884238cca7e0cd26949e81fe8f3a3cc394
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-06-25 03:30:27 +0800

    Add name mutator.

--- app/WilayahTelekomunikasi.php
+++ app/WilayahTelekomunikasi.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class WilayahTelekomunikasi extends Model
 {
     /**
+     * Set the user's first name.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtoupper($value);
+    }
+
+    /**
      * A wilayah telekomunikasi can have many assets.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
