4b8a5324d68272b941e9e2af90f6ffe91f9a3e2a
hananloser <hasyrawi@gmail.com>
2020-07-16 21:16:35 +0800

    make resource and more stuff

---
+++ database/factories/AsuranceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Asurance;
+use Faker\Generator as Faker;
+
+$factory->define(Asurance::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
