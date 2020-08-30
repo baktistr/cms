57d1b1cb880c9ed3e57cc062885039b88b1f4b0d
luck_nut <hasyrawi@gmail.com>
2020-08-31 02:25:11 +0800

    create new resourcec for space

---
+++ app/Http/Resources/BuildingSpaceResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BuildingSpaceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'is_available'  => $this->is_available,
+            'name'          => $this->name,
+            'desc'          => $this->description,
+            'building'      => AssetResoure::make($this->building)
+        ];
+    }
+}
