52ad14551908bfd6ead0f4d82e32b8f07052ae39
luck_nut <hasyrawi@gmail.com>
2020-09-04 12:41:55 +0800

    Syncronize on Migration

--- app/Http/Resources/BuildingResoure.php
+++ app/Http/Resources/BuildingResoure.php
@@ -18,23 +18,12 @@ class BuildingResoure extends JsonResource
         return [
             'id'             => $this->id,
             'name'           => $this->name,
-            'type'           => $this->type,
-            'description'    => $this->description,
-            'address'        => $this->address_detail,
-            'latitude'       => $this->latitude,
-            'longitude'      => $this->longitude,
-            'numberOfFloors' => $this->number_of_floors,
-            'price'          => $this->when($this->type === 'sale', $this->price),
-            'formattedPrice' => $this->when($this->type === 'sale', $this->formatted_price),
-            'province'       => ProvinceResource::make($this->province),
-            'regency'        => RegencyResource::make($this->regency),
-            'district'       => DistrictResource::make($this->district),
-            'images'         => BuildingImageResource::collection(collect($this->getMedia('image'))),
             'allotment'      => $this->allotment,
             'phone_number'   => $this->phone_number,
             'surface_area'   => $this->surface_area,
+            'images'         => BuildingImageResource::collection(collect($this->getMedia('image'))),
             'area'           => AreaResource::make($this->area),
-            'manager'        => UserResource::make($this->pic),      
+            'manager'        => UserResource::make($this->pic),     
         ];
     }
 }
